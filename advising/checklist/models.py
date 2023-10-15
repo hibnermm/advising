2108f67e781738469036af86d9fea19289d9faf6
J.J. Hibner <jjmhibner@gmail.com>
2023-10-15 00:32:25 -0700

    update models, views, and degree_list template

--- advising/checklist/models.py
+++ advising/checklist/models.py
@@ -1,104 +1,79 @@
 from django.db import models
 
 # Create your models here.
-class College(models.Model):
-  college_name = models.CharField(max_length=200)
-  def __str__(self):
-    return self.college_name
-  
 class Department(models.Model):
-  dept_name = models.CharField(max_length=200, verbose_name="department")   
-  college = models.ForeignKey(College, on_delete=models.CASCADE)
+  name = models.CharField(max_length=200, verbose_name="department")   
   def __str__(self):
-    return self.dept_name
-
-class Degree(models.Model):
-  degree_name = models.CharField(max_length=200)   
-  degree_abbrev = models.CharField(max_length=20, verbose_name="degree abbreviation")   
-  college = models.ForeignKey(College, on_delete=models.CASCADE)
-  fields = models.ManyToManyField("Field", verbose_name="Field of Study", through="DegreeField", null=True)
-  def __str__(self):
-    return self.degree_name
-
-class Field(models.Model):
-  field_name = models.CharField(max_length=200, verbose_name="field of study")  
-  field_abbrev = models.CharField(max_length=50, verbose_name="field of study abbreviation") 
-  department = models.ForeignKey(Department, on_delete=models.CASCADE) 
-  def __str__(self):
-    return self.field_name
-
-class DegreeField(models.Model):
-  degree = models.ForeignKey(Degree, on_delete=models.CASCADE) 
-  field = models.ForeignKey(Field, verbose_name="field of study", on_delete=models.CASCADE) 
-  degreefieldno = models.IntegerField(verbose_name="degree field of study number")
-  degree_minhours = models.IntegerField(verbose_name="degree minimum hours required")
-  field_minhours = models.IntegerField(verbose_name="field of study minimum hours required")
-  field_maxhours = models.IntegerField(verbose_name="field of study maximum hours")
-  def __str__(self):
-    return"{}_{}".format(self.degree.__str__(), self.field.__str())
-
-
-class Requirement(models.Model):
-  req_domain_name = models.CharField(verbose_name="name of required domain", max_length=200)
-  req_domain_abbrev = models.CharField(verbose_name="abbreviation of required domain", max_length=20)
-  types = models.ManyToManyField('Type', through="ReqType")
-
-class Type(models.Model):
-  type_name = models.CharField(verbose_name="name of required domain type", max_length=200)
-  type_abbrev = models.CharField(verbose_name="abbreviation of requirement type", max_length=20, )
-
-class ReqType(models.Model):
-  req = models.ForeignKey(Requirement, verbose_name=" required domain", on_delete=models.CASCADE)
-  type = models.ForeignKey(Type, verbose_name=" required domain type", null=True, on_delete=models.CASCADE) 
-
-class DegreeFieldReq(models.Model):
-  req_totalhours = models.IntegerField(verbose_name="required domain total hours")
-  type_totalhours = models.IntegerField(verbose_name="required domain type total hours")
-  degreefield = models.ForeignKey(DegreeField,on_delete=models.CASCADE)
-  courses = models.ManyToManyField('Course', through="ReqCourses")
-  reqtype = models.ForeignKey(ReqType, on_delete=models.CASCADE)
-
-                                        
-
+    return self.name
 
 class Subject(models.Model):                        
-  subj_name = models.CharField(max_length=200, verbose_name="subject name")  
-  subj_abbrev = models.CharField(max_length=20, verbose_name="subject abbreviation")  
-  department = models.ForeignKey(Department, on_delete=models.CASCADE)
+  name = models.CharField(max_length=200, verbose_name="subject name")  
+  abbrev = models.CharField(max_length=20, verbose_name="subject abbreviation")  
+  dept = models.ForeignKey(Department, verbose_name="department", on_delete=models.CASCADE)
   def __str__(self):
-    return self.subject_abbrev
+    return self.abbrev
+
+class Field(models.Model):
+  name = models.CharField(max_length=200, verbose_name="field of study")  
+  abbrev = models.CharField(max_length=50, verbose_name="field of study abbreviation", null=True) 
+  dept = models.ForeignKey(Department, verbose_name="department", on_delete=models.CASCADE) 
+  traininglevels = models.ManyToManyField("TrainingLevel", through="Degree")
+  def __str__(self):
+    return self.name
+
+class SubField(models.Model):
+  name = models.CharField(max_length=20, verbose_name="specialization name for field of study")
+  abbrev = models.CharField(max_length=20, verbose_name="specialization abbreviation for field of study", null=True)
+  field = models.ForeignKey(Field, verbose_name="field of study", on_delete=models.CASCADE)
+  def __str__(self):
+    return self.abbrev
+
+class TrainingLevel(models.Model):
+  name = models.CharField(max_length=200, verbose_name="training level name")   
+  abbrev = models.CharField(max_length=20, verbose_name="training level abbreviation")   
+  def __str__(self):
+    return self.abbrev
+
+class Degree(models.Model): 
+  field = models.ForeignKey(Field, on_delete=models.CASCADE)
+  traininglevel = models.ForeignKey(TrainingLevel, on_delete=models.CASCADE)
+  code = models.IntegerField(verbose_name="degree code number")
+  minhours = models.IntegerField(verbose_name="degree minimum hours required")
+  field_minhours = models.IntegerField(verbose_name="field minimum hours required")
+  field_maxhours = models.IntegerField(verbose_name="field maximum hours")
+  def __str__(self):
+    return "{} {}".format(self.traininglevel.__str__(), self.field.__str__())
+
+class Requirement(models.Model):
+  name = models.CharField(verbose_name="name of requirement domain", max_length=200)
+  abbrev = models.CharField(verbose_name="abbreviation of requirement domain", max_length=20)
+  totalhours = models.IntegerField(verbose_name="required domain total hours")
+  def __str__(self):
+    return self.name
+
+class ReqType(models.Model):
+  name = models.CharField(verbose_name="name of requirement domain-type", max_length=200)
+  abbrev = models.CharField(verbose_name="abbreviation of requirement domain-type", null=True, max_length=20)
+  totalhours = models.IntegerField(verbose_name="required domain type total hours")
+  totalno_courses = models.IntegerField(verbose_name="total number of courses")
+  req = models.ForeignKey(Requirement, on_delete=models.CASCADE) 
+  courses = models.ManyToManyField("Course", through="ReqCourse")
+  def __str__(self):
+      return "{}: {}".format(self.req.__str__(), self.name)
 
 class Course(models.Model):
-  course_name = models.CharField(max_length=200)          
-  course_no = models.IntegerField(verbose_name="course number")
-  course_hours = models.IntegerField(verbose_name="course hours")
+  name = models.CharField(verbose_name="course name", max_length=200)          
+  no = models.IntegerField(verbose_name="course number")
+  hours = models.IntegerField(verbose_name="course hours")
   subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-  department = models.ForeignKey(Department, on_delete=models.CASCADE)
+  prereqs = models.ManyToManyField('Prereq', through='CoursePrereq')
+  semesters = models.ManyToManyField("Semester", through="CourseSched")
   def __str_(self):
-    return "{} {}: {}".format(self.subj_abbrev.__str__(), self.course_no.__str__(), self.course_name.__str__())
+    return "{} {}: {}".format(self.subject.__str__(), self.no, self.name)
 
-
-class ReqCourses(models.Model):
-  total_courses = models.IntegerField(verbose_name="total number of courses required")
-  course_required = models.BooleanField(null=False, verbose_name="is course required?")
-  degreefieldreq = models.ForeignKey(DegreeFieldReq,on_delete=models.CASCADE)
-  course = models.ForeignKey(Course,on_delete=models.CASCADE)
-
-
-class Equiv(models.Model):
-  equiv_transfcode = models.IntegerField(verbose_name="equivalent transfer course code")
-  equiv_transfname = models.CharField(max_length=200, verbose_name="equivalent transfer course name")
-  course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-class Prereq(models.Model):
-  prereq_name = models.CharField(max_length=200, verbose_name="prerequisite course name")          
-  prereq_no = models.IntegerField(verbose_name="prerequisite course number")
-  prereq_hours = models.IntegerField(verbose_name="prerequisite course hours")
-
-
-class CoursePrereq(models.Model):
-  equiv = models.ForeignKey(Equiv, verbose_name="equivalent transfer course", max_length=200, on_delete=models.CASCADE)
-  prereq = models.ForeignKey(Prereq, verbose_name="prerequisite course", max_length=200, on_delete=models.CASCADE)
+class ReqCourse(models.Model):
+  reqtype = models.ForeignKey(ReqType,on_delete=models.CASCADE)    
+  course= models.ForeignKey(Course,on_delete=models.CASCADE)
 
 
 class Semester(models.Model):
@@ -117,21 +92,31 @@ class Semester(models.Model):
     (SUMMER1, "Summer1"),
     (SUMMER2, "Summer2")
   ]
-  season_name = models.CharField(choices=season_choices, max_length=20)
-  courses = models.ManyToManyField("Course", verbose_name="Field of Study", through="CourseSched")
+  season = models.CharField(choices=season_choices, max_length=20)
+  year = models.IntegerField(null=True, help_text="academic year")
+  courses = models.ManyToManyField('Course', through='CourseSched')
+  def __str__(self):
+     return "{} {}".format(self.season, self.year)
 
 class CourseSched(models.Model):
+  semester = models.ForeignKey('Semester', on_delete=models.CASCADE)
+  course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+
+class Equiv(models.Model):
+  transf_code = models.IntegerField(verbose_name="equivalent transfer course code")
+  transf_name = models.CharField(max_length=200, verbose_name="equivalent transfer course name")
   course = models.ForeignKey(Course, on_delete=models.CASCADE)
-  semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
-  yearstart = models.IntegerField(null=True, help_text="start year of course")
+  def __init__(self):
+    return "{} {}".format(self.name, self.code)
 
+class Prereq(models.Model):
+  prereq_name = models.CharField(max_length=200, verbose_name="prerequisite course name")          
+  prereq_no = models.IntegerField(verbose_name="prerequisite course number")
+  prereq_hours = models.IntegerField(verbose_name="prerequisite course hours")
+  def __init__(self):
+    self.name
 
-"""
-
-ERRORS:
-checklist.DegreeField: (fields.E336) The model is used as an intermediate model by 'checklist.Semester.courses', but it does not have a foreign key to 'Semester' or 'Course'.
-"""
-
-
-  
-  
+class CoursePrereq(models.Model):
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+  prereq = models.ForeignKey(Prereq, verbose_name="prerequisite course", on_delete=models.CASCADE)
--- advising/checklist/views.py
+++ advising/checklist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Degree, Field #?
+from .models import Department, Field, TrainingLevel
 
 # Create your views here.
 def index(request):
@@ -10,12 +10,19 @@ def degree_search(request):
   return render(request, "checklist/degree_search.html", {"search_text": search_text})     #not working
 
 def degree_list(request):
-  degrees = Degree.objects.all().order_by("degree_abbrev")
-  fields = Degree.objects.prefetch_related('field')  #get field table for every item in degree query set; only shows BBA
-  #departments = Department.objects.all()  no data
+  trainings= TrainingLevel.objects.all()
+  fields = Field.objects.all()
+  depts = Department.objects.all()
   context = {
-        'degrees': degrees,
+        'trainings': trainings,
         'fields': fields,
-        #'departments': departments
+        'depts': depts
     }
   return render(request, 'checklist/degree_list.html', context)
+"""
+field = Field.objects.all()
+field.get(name__contains="computer")   #where %like%
+field.filter(dept__name="Department of Life, Earth and Environmental Sciences")   #spans relationships
+field.filter(department__name="Department of Life, Earth and Environmental Sciences")     #doesn't work
+field.filter(traininglevels__abbrev="BBA")    #works
+"""
